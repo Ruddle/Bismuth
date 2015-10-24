8edf213a80bac662fb5eda7b699338b440d7f85f
Thomas Simon <thomas.simon.work@gmail.com>
2015-10-24 23:13:15 +0200

    Mesh

--- Bismuth/Glew_SDL2/Scene_SDL.cpp
+++ Bismuth/Glew_SDL2/Scene_SDL.cpp
@@ -21,8 +21,8 @@ Scene_SDL::Scene_SDL(int w, int h)
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
 	cout << "Echec : " << endl << SDL_GetError() << endl;
 	
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
