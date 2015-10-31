960ffc26b372aabe7bf5180ec6f5252aaa68cf89
Thomas Simon <thomas.simon.work@gmail.com>
2015-10-31 21:38:09 +0100

    Revert "Camera"
    This reverts commit 34e9b5a450fea543c0d4974af4e218db97c35111.

--- Bismuth/Glew_SDL2/Camera.h
+++ Bismuth/Glew_SDL2/Camera.h
@@ -6,13 +6,13 @@
 class Camera
 {
 public:
-	Camera(glm::mat4 &projection);
+	Camera(glm::mat4 &projection, PhysicComponent* physicComponent);
 	~Camera();
 
-	
+	PhysicComponent*  getPhysicComponent(void) { return mPhysicComponent; }
 
 private:
-	
+	PhysicComponent* mPhysicComponent;
 	glm::mat4 mProjection;
 	glm::mat4 mView;
 
