966ce600431a1e7dbe3d38069d5eb339bd76ac59
Thomas Simon <thomas.simon.work@gmail.com>
2015-11-14 18:44:17 +0100

    re

--- Bismuth/Glew_SDL2/DetectionProcessor.h
+++ Bismuth/Glew_SDL2/DetectionProcessor.h
@@ -11,21 +11,23 @@ public:
 	DetectionProcessor();
 	~DetectionProcessor();
 
-	Contact* detection(Shape a, Shape b, glm::vec3, glm::vec3 pos1, glm::vec3 rot1, glm::vec3 pos2, glm::vec3 rot2);
+	Contact* detection(DetectionComponent::Shape a, DetectionComponent::Shape b,
+	    glm::vec3 pos1, glm::vec3 rot1,
+		glm::vec3 pos2, glm::vec3 rot2);
 
-	Contact* cubeToRay();
-	Contact* cubeToSphere();
-	Contact* cubeToPlane();
-	Contact* cubeToCube();
+	Contact* cubeToRay(CubeDetectionComponent cube, RayDetectionComponent ray);
+	Contact* cubeToSphere(CubeDetectionComponent cube, SphereDetectionComponent sphere);
+	Contact* cubeToPlane(CubeDetectionComponent cube, PlaneDetectionComponent plane);
+	Contact* cubeToCube(CubeDetectionComponent cube, CubeDetectionComponent cube2 );
 
-	Contact* planeToRay();
-	Contact* planeToSphere();
-	Contact* planeToPlane();
+	Contact* planeToRay(PlaneDetectionComponent plane, RayDetectionComponent ray);
+	Contact* planeToSphere(PlaneDetectionComponent plane, SphereDetectionComponent sphere);
+	Contact* planeToPlane(PlaneDetectionComponent plane, PlaneDetectionComponent plane2);
 
-	Contact* sphereToRay();
-	Contact* sphereToSphere();
+	Contact* sphereToRay(SphereDetectionComponent sphere, RayDetectionComponent ray);
+	Contact* sphereToSphere(SphereDetectionComponent sphere, SphereDetectionComponent sphere2);
 
-	Contact* rayToRay();
+	Contact* rayToRay(RayDetectionComponent ray, RayDetectionComponent ray2);
 
 	
 
