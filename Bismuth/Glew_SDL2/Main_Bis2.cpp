640b3db1d9aebbd50a5351d1be8b6175536decc7
Thomas Simon <thomas.simon.work@gmail.com>
2015-11-14 15:39:28 +0100

    re

--- Bismuth/Glew_SDL2/Main_Bis2.cpp
+++ Bismuth/Glew_SDL2/Main_Bis2.cpp
@@ -25,11 +25,11 @@ using namespace glm;
 using namespace std;
 
 
-//FILE _iob[] = { *stdin, *stdout, *stderr };
-//extern "C" FILE * __cdecl __iob_func(void)
-//{
-//	return _iob;
-//}
+FILE _iob[] = { *stdin, *stdout, *stderr };
+extern "C" FILE * __cdecl __iob_func(void)
+{
+	return _iob;
+}
 
 
 
