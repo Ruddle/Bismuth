82b20cb1d4ea6213fb04477f6d23af8d9c397731
Askannz <aska990@hotmail.fr>
2016-02-28 00:35:19 +0100

    méthode add

--- Bismuth/Glew_SDL2/Entity.h
+++ Bismuth/Glew_SDL2/Entity.h
@@ -13,8 +13,10 @@ public:
 	virtual Type getType() const = 0;
 	void setId(int id) {mId = id;}
 	int getId() {return mId;}
+	void add(Entity *entity) { mChildren.push_back(entity); }
 
 private:
     int mId;
 	glm::mat4 mModel;
+	std::vector<Entity*> mChildren;
 };
