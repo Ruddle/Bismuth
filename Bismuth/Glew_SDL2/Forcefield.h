d2ab8b5d208d8483dc14994356ae003dd5aee58c
Askannz <aska990@hotmail.fr>
2016-02-28 13:59:28 +0100

    Re

--- Bismuth/Glew_SDL2/Forcefield.h
+++ Bismuth/Glew_SDL2/Forcefield.h
@@ -13,7 +13,7 @@ class Forcefield : public Updatable
         Forcefield(DetectionComponent *mDetectionComponent);
         ~Forcefield();
         DetectionComponent* getDetectionComponent() { return mDetectionComponent; }
-        virtual oid force(glm::vec3 pos) const = 0;
+        virtual void force(glm::vec3 pos) const = 0;
 
 
     protected:
